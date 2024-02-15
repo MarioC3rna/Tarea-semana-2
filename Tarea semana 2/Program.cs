f9cd61a1e98a8cba3158831afd02a4490a7cb30b
MARIO FERNANDO CERNA NAJERA <mcernan@miumg.edu.gt>
2024-02-15 11:29:03 -0600

    Tercero

--- Tarea semana 2/Program.cs
+++ Tarea semana 2/Program.cs
@@ -1,67 +1,61 @@
-﻿Console.WriteLine("Uso de char y string");
+﻿Console.WriteLine("Operaciones con tipos de datos");
 Console.WriteLine("");
 
-//Texto a Mayusculas
-string nota1, notaM;
-Console.WriteLine("---Ingrese su texto para que lo imprima en Mayuscula---");
-Console.WriteLine("");
-Console.WriteLine("Ingrese su texto aqui en Minuscula");
-nota1 = Console.ReadLine();
-notaM = nota1.ToUpper();
-Console.WriteLine("Este es su texto " + notaM);
+//Suma
+int n1, n2;
+int resultado1;
+Console.WriteLine("---Ingrese los numeros enteros que quiere Sumar---");
+Console.WriteLine("Ingrese primer numero");
+n1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese segundo numero");
+n2 = int.Parse(Console.ReadLine());
+resultado1 = n1 + n2;
+Console.WriteLine("El resultado es " + resultado1);
 
-//Imprime en Minusculas 
-string nota2, notam;
+//Resta
+decimal n3, n4, resultado2;
 Console.WriteLine("");
-Console.WriteLine("--Ingrese su texto para que lo imprima en Minusculas--");
-Console.WriteLine("");
-Console.WriteLine("Ingrese su texto aqui en Mayusculas");
-nota2 = Console.ReadLine();
-notam = nota2.ToLower();
-Console.WriteLine("Este es su texto " + notam);
-
-//Imprime al reves 
-string nota3;
-Console.WriteLine("");
-Console.WriteLine("--Ingrese su texto que quire imprimir al reves--");
-Console.WriteLine("");
-Console.WriteLine("Ingrese su texto aqui su texto");
-nota3 = Console.ReadLine();
-char[] notaInv = nota3.ToCharArray();
-Array.Reverse(notaInv);
-string notaNueva = new string(notaInv);
-Console.WriteLine("Este es su texto " + notaNueva);
-
-//Buscardor
-string nota4;
-Console.WriteLine("");
-Console.WriteLine("---Ingrese su cadena de texto---");
-Console.WriteLine("");
-nota4 = Console.ReadLine();
-Console.WriteLine("Ingrese la palabra que quiere buscar");
-string Pbuscar = Console.ReadLine();
-string notaMinus = nota4.ToLower();
-bool Pbuscada = nota4.Contains(notaMinus);
+Console.WriteLine("---Ingrese los numeros enteros que quiere Restar---");
+Console.WriteLine("Ingrese primer numero");
+n3 = decimal.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese segundo numero");
+n4 = decimal.Parse(Console.ReadLine());
+resultado2 = n3 - n4;
+Console.WriteLine("El resultado es " + resultado2);
 
 
 
-if (Pbuscada)
-{
-
-    Console.WriteLine("La palabra " + Pbuscar + " Fue encontrada ");
-}
-else
-{
-
-    Console.WriteLine("La palabra" + Pbuscar + "No fue encontrada en el texto ");
-}
-
-//Contar las Vocales 
-string nota5;
+//Multipliacion
+float n5, n6, resultado3;
 Console.WriteLine("");
-Console.WriteLine("--Ingrese su texto del cual quiere contar las vocales--");
+Console.WriteLine("---Ingrese los numeros flotantes que quiere Multiplicar---");
+Console.WriteLine("Ingrese primer numero");
+n5 = float.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese segundo numero");
+n6 = float.Parse(Console.ReadLine());
+resultado3 = n5 * n6;
+Console.WriteLine("El resultado es " + resultado3);
+
+//Division
+int n7, n8, resultado4;
 Console.WriteLine("");
-Console.WriteLine("Ingrese su texto aqui ");
-nota5 = Console.ReadLine();
-int Cvocales = nota5.Count(c => "aeiouAEIOU".Contains(c));
-Console.WriteLine("Este es la cantidad de vocales que tiene su texto " + Cvocales);
+Console.WriteLine("---Ingrese los numeros enteros que quiere Dividir---");
+Console.WriteLine("Ingrese primer numero");
+n7 = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese segundo numero");
+n8 = int.Parse(Console.ReadLine());
+resultado4 = n7 / n8;
+Console.WriteLine("El resultado es " + resultado4);
+
+//Modulo de dos numeros enteros
+int n9, n0, resultado5;
+Console.WriteLine("");
+Console.WriteLine("---Ingrese los numeros enteros que quiere ver su modulo---");
+Console.WriteLine("Ingrese primer numero");
+n9 = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese segundo numero");
+n0 = int.Parse(Console.ReadLine());
+resultado5 = n9 % n0;
+Console.WriteLine("El resultado de" + n9 + "entre" + n0 + "es el modulo " + resultado5);
+
+
